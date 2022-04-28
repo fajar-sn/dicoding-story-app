7d625f293ae8639d8e71365836502bfd6ed40638
Fajar Septian Nugraha <fajarsn99@gmail.com>
2022-04-29 05:00:17 +0700

    Created maps activity and story location

--- app/src/main/java/com/bangkit/intermediate/dicodingstoryapp/ui/story/detail/StoryDetailActivity.kt
+++ app/src/main/java/com/bangkit/intermediate/dicodingstoryapp/ui/story/detail/StoryDetailActivity.kt
@@ -1,27 +1,45 @@
 package com.bangkit.intermediate.dicodingstoryapp.ui.story.detail
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.bangkit.intermediate.dicodingstoryapp.data.remote.response.Story
 import com.bangkit.intermediate.dicodingstoryapp.databinding.ActivityStoryDetailBinding
+import com.bangkit.intermediate.dicodingstoryapp.ui.story.map.StoryLocationActivity
 import com.bumptech.glide.Glide
 
 class StoryDetailActivity : AppCompatActivity() {
+    private lateinit var story: Story
+    private lateinit var locationButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityStoryDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupData(binding)
+        setupAction()
     }
 
     private fun setupData(binding: ActivityStoryDetailBinding) {
-        val story = intent.getParcelableExtra<Story>("Story") as Story
+        story = intent.getParcelableExtra<Story>(EXTRA_STORY) as Story
         binding.textViewNameStoryDetail.text = story.name
         binding.textViewDescriptionStoryDetail.text = story.description
+        locationButton = binding.locationButton
 
         Glide.with(applicationContext)
             .load(story.photoUrl)
             .circleCrop()
             .into(binding.imageViewStoryDetail)
+    }
+
+    private fun setupAction() = locationButton.setOnClickListener {
+        val intent = Intent(this, StoryLocationActivity::class.java)
+        intent.putExtra(EXTRA_STORY, story)
+        startActivity(intent)
+    }
+
+    companion object {
+        const val EXTRA_STORY = "EXTRA_STORY"
     }
 }
